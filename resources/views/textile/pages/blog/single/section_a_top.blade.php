c178ac9e556d1909adb8b9ba7730e1025094b1f5
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2021-04-09 23:27:13 +0100

    add Transable system to Post Model

--- resources/views/textile/pages/blog/single/section_a_top.blade.php
+++ resources/views/textile/pages/blog/single/section_a_top.blade.php
@@ -1,13 +1,19 @@
-<div class="page-header text-center" style="background-image: url('assets/images/page-header-bg.jpg')">
+<div class="page-header text-center" 
+    @if(setting('pages.background_cover'))
+      style="background-image: url('{{Voyager::image(setting('pages.background_cover'))}}')"
+    @endif
+>
     <div class="container">
-        <h1 class="page-title">{{$post->field('title')}}<span>Single Post</span></h1>
+        <h1 class="page-title">{{$post->field('title')}}
+            {{--<span>Single Post</span>--}}
+        </h1>
     </div><!-- End .container -->
 </div><!-- End .page-header -->
 <nav aria-label="breadcrumb" class="breadcrumb-nav mb-3">
     <div class="container">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
-            <li class="breadcrumb-item"><a href="{{route('blog')}}">Blog</a></li>
+            <li class="breadcrumb-item"><a href="{{route('home')}}">{{__('navbar.home')}}</a></li>
+            {{--<li class="breadcrumb-item"><a href="{{route('blog')}}">{{__('navbar.home')}}</a></li>--}}
             <li class="breadcrumb-item active" aria-current="page">{{$post->field('title')}}</li>
         </ol>
     </div><!-- End .container -->
