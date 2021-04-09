375f329c238864bd9c662221f207793ae0d2df3d
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2021-04-09 23:34:38 +0100

    make same changes in Header

--- resources/views/textile/layouts/sections/mobileNavbar.blade.php
+++ resources/views/textile/layouts/sections/mobileNavbar.blade.php
@@ -35,9 +35,8 @@
 
             <div class="social-icons">
                 <a href="{{setting('social.facebook')}}" class="social-icon social-facebook" title="Facebook" target="_blank"><i class="icon-facebook-f"></i></a>
-                <a href="{{setting('social.twitter')}}" class="social-icon social-twitter" title="Twitter" target="_blank"><i class="icon-twitter"></i></a>
                 <a href="{{setting('social.instagram')}}" class="social-icon social-instagram" title="Instagram" target="_blank"><i class="icon-instagram"></i></a>
-                <a href="{{setting('social.youtube')}}" class="social-icon social-youtube" title="Youtube" target="_blank"><i class="icon-youtube"></i></a>
+                <a href="{{setting('social.linkedin')}}" class="social-icon social-linkedin" title="linkedin" target="_blank"><i class="icon-linkedin"></i></a>
             </div>
         </div>
     </div>
