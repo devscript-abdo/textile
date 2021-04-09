6cca244c1e2b0f8c8fdbef5cef344f5a9fa09d73
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2021-04-09 14:13:58 +0100

    make same chnages in the query builder

--- resources/views/textile/pages/about/about_a/section_b_values.blade.php
+++ resources/views/textile/pages/about/about_a/section_b_values.blade.php
@@ -1,41 +1,40 @@
 <div class="container mt-2">
-    <h2 class="title">Who We Are</h2>
+    <h2 class="title">{{__('about.values')}} :</h2>
 <div class="row justify-content-center">
-   
-                        <div class="col-lg-4 col-sm-6">
-                            <div class="icon-box icon-box-sm text-center">
-                                <span class="icon-box-icon">
-                                    <i class="icon-puzzle-piece"></i>
-                                </span>
-                                <div class="icon-box-content">
-                                    <h3 class="icon-box-title">Design Quality</h3><!-- End .icon-box-title -->
-                                    <p>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero <br>eu augue.</p>
-                                </div><!-- End .icon-box-content -->
-                            </div><!-- End .icon-box -->
-                        </div><!-- End .col-lg-4 col-sm-6 -->
+                       <div class="col-lg-4 col-sm-6">
+                           <div class="icon-box icon-box-sm text-center">
+                               <span class="icon-box-icon">
+                                   <i class="icon-puzzle-piece"></i>
+                               </span>
+                               <div class="icon-box-content">
+                                   <h3 class="icon-box-title">{{__('about.value_excelence')}}</h3><!-- End .icon-box-title -->
+                                   <p>{{__('about.value_excelence_description')}}</p>
+                               </div><!-- End .icon-box-content -->
+                           </div><!-- End .icon-box -->
+                       </div><!-- End .col-lg-4 col-sm-6 -->
 
-                        <div class="col-lg-4 col-sm-6">
-                            <div class="icon-box icon-box-sm text-center">
-                                <span class="icon-box-icon">
-                                    <i class="icon-life-ring"></i>
-                                </span>
-                                <div class="icon-box-content">
-                                    <h3 class="icon-box-title">Professional Support</h3><!-- End .icon-box-title -->
-                                    <p>Praesent dapibus, neque id cursus faucibus, <br>tortor neque egestas augue, eu vulputate <br>magna eros eu erat. </p>
-                                </div><!-- End .icon-box-content -->
-                            </div><!-- End .icon-box -->
-                        </div><!-- End .col-lg-4 col-sm-6 -->
+                       <div class="col-lg-4 col-sm-6">
+                           <div class="icon-box icon-box-sm text-center">
+                               <span class="icon-box-icon">
+                                   <i class="icon-life-ring"></i>
+                               </span>
+                               <div class="icon-box-content">
+                                   <h3 class="icon-box-title">{{__('about.value_qualite')}}</h3><!-- End .icon-box-title -->
+                                   <p>{{__('about.value_qualite_description')}} </p>
+                               </div><!-- End .icon-box-content -->
+                           </div><!-- End .icon-box -->
+                       </div><!-- End .col-lg-4 col-sm-6 -->
 
-                        <div class="col-lg-4 col-sm-6">
-                            <div class="icon-box icon-box-sm text-center">
-                                <span class="icon-box-icon">
-                                    <i class="icon-heart-o"></i>
-                                </span>
-                                <div class="icon-box-content">
-                                    <h3 class="icon-box-title">Made With Love</h3><!-- End .icon-box-title -->
-                                    <p>Pellentesque a diam sit amet mi ullamcorper <br>vehicula. Nullam quis massa sit amet <br>nibh viverra malesuada.</p> 
-                                </div><!-- End .icon-box-content -->
-                            </div><!-- End .icon-box -->
-                        </div><!-- End .col-lg-4 col-sm-6 -->
-                    </div><!-- End .row -->
-    </div><!-- End .row -->
+                       <div class="col-lg-4 col-sm-6">
+                           <div class="icon-box icon-box-sm text-center">
+                               <span class="icon-box-icon">
+                                   <i class="icon-heart-o"></i>
+                               </span>
+                               <div class="icon-box-content">
+                                   <h3 class="icon-box-title">{{__('about.value_innovation')}}</h3><!-- End .icon-box-title -->
+                                   <p>{{__('about.value_innovation_description')}}</p> 
+                               </div><!-- End .icon-box-content -->
+                           </div><!-- End .icon-box -->
+                       </div><!-- End .col-lg-4 col-sm-6 -->
+                   </div><!-- End .row -->
+   </div><!-- End .row -->
--- resources/views/textile/pages/about/about_a/section_d_who.blade.php
+++ resources/views/textile/pages/about/about_a/section_d_who.blade.php
@@ -1,26 +1,27 @@
 {{--<div class="bg-light-2 pt-6 pb-5 mb-6 mb-lg-8">--}}
-<div class="">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6">
-                {{--<h2 class="title">Who We Are</h2>--}}
-                {{--<p class="lead text-primary mb-3">Pellentesque</p>--}}
-                <p class="mb-2">
-                    {!!$pages['about']->body!!}
-                </p>
-
-                <a href="{{route('products')}}" class="btn btn-sm btn-minwidth btn-outline-primary-2 mt-2">
-                    <span>{{__('navbar.shope')}}</span>
-                    <i class="icon-long-arrow-right"></i>
-                </a>
-            </div>
-
-            <div class="col-lg-6">
-                <div class="about-images">
-                    <img src="{{$pages['about']->photo}}" alt="{{$pages['about']->field('title')}}" class="about-img-front">
-                    <img src="{{asset('assets/images/about/img-2.jpg')}}" alt="" class="about-img-back">
+    <div class="">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 mb-3 mb-lg-0">
+                    {{--<h2 class="title">Who We Are</h2>--}}
+                    {{--<p class="lead text-primary mb-3">Pellentesque</p>--}}
+                    <p class="mb-2">
+                        {!!$pages['about']->body!!}
+                    </p>
+    
+                    <a href="{{route('products')}}" class="btn btn-sm btn-minwidth btn-outline-primary-2 mt-2">
+                        <span>{{__('navbar.shope')}}</span>
+                        <i class="icon-long-arrow-right"></i>
+                    </a>
+                </div>
+    
+                <div class="col-lg-4 ">
+                    <div class="about-images">
+                        <img src="{{$pages['about']->photo}}" loading="lazy" alt="{{$pages['about']->field('title')}}" class="about-img-front">
+                        {{--<img src="{{$pages['about']->photo}}" loading="lazy" alt="{{$pages['about']->field('title')}}" class="about-img-back">--}}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    
