43b957016c774da2cf560779dcfea34d7a048ac1
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2021-04-09 22:42:00 +0100

    add social Media Url

--- resources/views/textile/pages/home/section_e_social_shop.blade.php
+++ resources/views/textile/pages/home/section_e_social_shop.blade.php
@@ -8,9 +8,9 @@
 
                     <div class="social-icons social-icons-colored justify-content-center">
                         <a href="{{setting('social.facebook')}}" class="social-icon social-facebook" title="Facebook" target="_blank"><i class="icon-facebook-f"></i></a>
-                        <a href="{{setting('social.twitter')}}" class="social-icon social-twitter" title="Twitter" target="_blank"><i class="icon-twitter"></i></a>
+                        {{--<a href="{{setting('social.twitter')}}" class="social-icon social-twitter" title="Twitter" target="_blank"><i class="icon-twitter"></i></a>--}}
                         <a href="{{setting('social.instagram')}}" class="social-icon social-instagram" title="Instagram" target="_blank"><i class="icon-instagram"></i></a>
-                        <a href="{{setting('social.youtube')}}" class="social-icon social-youtube" title="Youtube" target="_blank"><i class="icon-youtube"></i></a>
+                        <a href="{{setting('social.linkedin')}}" class="social-icon social-linkedin" title="linkedin" target="_blank"><i class="icon-linkedin"></i></a>
                         {{--<a href="#" class="social-icon social-pinterest" title="Pinterest" target="_blank"><i class="icon-pinterest"></i></a>--}}
                     </div>
                 </div>
