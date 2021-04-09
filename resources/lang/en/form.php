ebb8f5a23bec676cf5927d7019c43f8f2ca72d3c
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2021-04-09 21:52:42 +0100

    change partner form  trans value

--- resources/lang/en/form.php
+++ resources/lang/en/form.php
@@ -19,12 +19,12 @@ return [
     'form_partner_title' => 'please fill out the following form',
     'form_partner_title_description' => 'veuillez remplir le formulaire suivant',
     /*---------------------------------------------------------*/
-    'form_partner_name' => 'Nom Complet',
+    'form_partner_name' => 'Full name',
     'form_partner_profession' => 'Profession',
-    'form_partner_ville' => 'Ville',
+    'form_partner_ville' => 'City',
     'form_partner_email' => 'E-mail',
-    'form_partner_phone' => 'Numéro de téléphone',
+    'form_partner_phone' => 'Phone number',
     'form_partner_subject' => 'Sujet',
-    'form_partner_message' => 'Votre demande',
-    'form_partner_button' => 'Envoyer'
+    'form_partner_message' => 'Request',
+    'form_partner_button' => 'Send'
 ];
